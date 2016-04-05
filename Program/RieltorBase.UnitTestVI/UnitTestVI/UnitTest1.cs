a379ae2a9cbae477f8d971f8d5324d345b3276b3
Gilleks <zhukov.vlad89@gmail.com>
2016-04-05 20:12:06 +0400

    Добавлен проект Unit тестов

---
+++ Program/RieltorBase.UnitTestVI/UnitTestVI/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VI_EF;
+
+namespace UnitTestVI
+{
+    [TestClass]
+    public class FirsrTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (VolgaInfoDBEntities context = new VolgaInfoDBEntities())
+            {
+                var newAgent = new Firm { Name = "xxx1" };
+
+                context.Firms.Add(newAgent);
+                context.SaveChanges();
+
+                Console.WriteLine("Done");
+            }
+        }
+    }
+}
