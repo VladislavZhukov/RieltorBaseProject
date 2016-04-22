be43736055d3c5bec025e260c6023a84d33e6593
Alexandr Ivanov <aaa@aaa.ru>
2016-04-22 19:50:48 +0400

    Добавлены функции добавления метаданных и созданы ссылки на главной странице

--- Program/RieltorBase.WebSite/Controllers/HomeController.cs
+++ Program/RieltorBase.WebSite/Controllers/HomeController.cs
@@ -1,18 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using RieltorBase.Domain;
 
 namespace RieltorBase.WebSite.Controllers
 {
     public class HomeController : Controller
     {
+        private VolgaInfoDBEntities context;
+
         //
         // GET: /Home/
         public ActionResult Index()
         {
             return View();
         }
-	}
+
+        public VolgaInfoDBEntities Context
+        {
+            get
+            {
+                return this.context 
+                    ?? (this.context = new VolgaInfoDBEntities());
+            }
+        }
+
+        public ActionResult CreateMetadata()
+        {
+            this.Context.CreateStandardMetadata();
+            this.Context.SaveChanges();
+
+            return this.RedirectToAction("Index");
+        }
+
+        public ActionResult ClearDatabase()
+        {
+            this.Context.ClearDatabase();
+            this.Context.SaveChanges();
+
+            return this.RedirectToAction("Index");
+        }
+
+        public ActionResult CreateTestObjects()
+        {
+            this.Context.CreateFewObjects();
+            this.Context.SaveChanges();
+
+            return this.RedirectToAction("Index");
+        }
+    }
 }
