2c49c190c6f6fb8ed47163f25907ae7667ece70b
n-demidov <you@example.com>
2016-05-05 14:57:34 +0400

    refactoring

--- Program/RieltorBase.WebSite/Scripts/inner/constants.js
+++ Program/RieltorBase.WebSite/Scripts/inner/constants.js
@@ -1,5 +1,5 @@
 ﻿'use strict';
 
 var API_V1 = '/api/v1/';
-var GET_REALTY_OBJECTS = API_V1 + 'realtyobjects';
+var GET_REALTY_OBJECTS = API_V1 + 'realtyobjects/';
 var GET_REALTY_OBJECT = API_V1 + 'realtyobjects/';
