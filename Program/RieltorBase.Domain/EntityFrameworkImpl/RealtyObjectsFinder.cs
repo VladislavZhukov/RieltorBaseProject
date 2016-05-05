318f99f821ace837acd309f35d0cd9b2a670848f
Alexandr Ivanov <aaa@aaa.ru>
2016-05-05 14:05:21 +0400

    Реализован упрощенный поиск по параметрам

--- Program/RieltorBase.Domain/EntityFrameworkImpl/RealtyObjectsFinder.cs
+++ Program/RieltorBase.Domain/EntityFrameworkImpl/RealtyObjectsFinder.cs
@@ -24,7 +24,16 @@
         /// <summary>
         /// Текущий запрос на поиск.
         /// </summary>
+        /// <remarks>Сюда могут добавляться условия до 
+        /// загрузки объектов из БД.</remarks>
         private IQueryable<RealtyObject> currentQuery;
+
+        /// <summary>
+        /// Текущий загруженный результат.
+        /// </summary>
+        /// <remarks>Нужен для фильтрации условий поиска, 
+        /// которые не удалось проверить до загрузки.</remarks>
+        private IEnumerable<RealtyObject> currentLoadedResult; 
 
         /// <summary>
         /// Создать экземпляр класса поиска объектов недвижимости.
@@ -46,6 +55,12 @@
                 throw new ArgumentNullException("initialQuery");
             }
 
+            if (string.IsNullOrWhiteSpace(options.RealtyObjectType))
+            {
+                throw new InvalidOperationException(
+                    "В параметрах поиска не указан тип объектов.");
+            }
+            
             this.options = options;
             this.initialQuery = initialQuery;
         }
@@ -58,22 +73,28 @@
         {
             this.currentQuery = this.initialQuery;
 
-            this.AddTypeCondition();
-            this.AddCostCondition();
-            this.AddDateCondition();
-            this.AddAddressCondition();
+            this.AddTypePreCondition();
+            this.AddCostPreCondition();
+            this.AddDatePreCondition();
+            this.AddAddressPreCondition();
 
-            List<RealtyObject> result = this.currentQuery.ToList();
+            this.currentLoadedResult = this.currentQuery.ToList();
+
+            this.AddCostPostCondition();
+            this.AddDatePostCondition();
+
+            ICollection<RealtyObject> result = this.currentLoadedResult.ToList();
 
             this.currentQuery = null;
+            this.currentLoadedResult = null;
 
             return result;
         }
 
         /// <summary>
-        /// Добавить условие поиска по типу.
+        /// Добавить условие поиска по типу (до загрузки объектов).
         /// </summary>
-        private void AddTypeCondition()
+        private void AddTypePreCondition()
         {
             if (!string.IsNullOrWhiteSpace(this.options.RealtyObjectType))
             {
@@ -83,50 +104,114 @@
         }
 
         /// <summary>
-        /// Добавить условие поиска по цене.
+        /// Добавить условие поиска по цене (до загрузки объектов).
         /// </summary>
-        private void AddCostCondition()
+        private void AddCostPreCondition()
         {
-            // todo
-            //if (this.options.MinCost != 0 || this.options.MaxCost != 0)
-            //{
-            //    this.currentQuery = this.currentQuery.Where(ro =>
-            //        ro.PropertyValues.Any(pv =>
-            //            pv.PropertyType.PropertyName == Metadata.CostPropName
-            //            /*&& pv.IntegerValue >= searchOptions.MinCost 
-            //             *&& pv.IntegerValue <= searchOptions.MaxCost*/));
-            //    // с ценой пока не работает!!! нужно целочисленное поле в БД.
-            //}
+            if (this.options.MinCost != 0 || this.options.MaxCost != 0)
+            {
+                this.currentQuery = this.currentQuery.Where(ro =>
+                    ro.PropertyValues.Any(pv =>
+                        pv.PropertyType.PropertyName == Metadata.CostPropName));
+            }
         }
 
         /// <summary>
-        /// Добавить условие поиска по дате.
+        /// Добавить условие поиска по дате (до загрузки объектов).
         /// </summary>
-        private void AddDateCondition()
+        private void AddDatePreCondition()
         {
-            // todo
-            //if (this.options.MinDate != null
-            //    || this.options.MaxDate != null)
-            //{
-            //    this.currentQuery = this.currentQuery.Where(ro =>
-            //        true);
-            //        // нужно обязательное поле "CreationDate" ro.CreationDate >= searchOptions.MinDate && ro.CreationDate <= searchOptions.MaxDate);
-            //}
+            if (this.options.MinDate != null
+                || this.options.MaxDate != null)
+            {
+                this.currentQuery = this.currentQuery.Where(ro =>
+                    ro.PropertyValues.Any(pv =>
+                        pv.PropertyType.PropertyName == Metadata.DatePropName));
+            }
         }
 
         /// <summary>
-        /// Добавить условие поиска по части адреса.
+        /// Добавить условие поиска по части адреса (до загрузки объектов).
         /// </summary>
-        private void AddAddressCondition()
+        private void AddAddressPreCondition()
         {
-            // todo
-            //if (!string.IsNullOrWhiteSpace(this.options.PartOfAddress))
-            //{
-            //    this.currentQuery = this.currentQuery.Where(ro =>
-            //        ro.PropertyValues.Any(pv =>
-            //            pv.PropertyType.PropertyName == "Адрес"
-            //            && pv.StringValue.Contains(this.options.PartOfAddress)));
-            //}
+            if (!string.IsNullOrWhiteSpace(this.options.PartOfAddress))
+            {
+                string addressPropName = Metadata.GetAddressPropName(
+                    this.options.RealtyObjectType);
+
+                this.currentQuery = this.currentQuery.Where(ro =>
+                    ro.PropertyValues.Any(pv =>
+                        pv.PropertyType.PropertyName == addressPropName
+                        && 
+                        (pv.StringValue.Contains(this.options.PartOfAddress))));
+            }
+        }
+
+        /// <summary>
+        /// Добавить условие поиска по цене (после загрузки объектов).
+        /// </summary>
+        private void AddCostPostCondition()
+        {
+            if (this.options.MinCost != 0 || this.options.MaxCost != 0)
+            {
+                this.currentLoadedResult = this.currentLoadedResult.Where(ob =>
+                    ob.PropertyValues.Any(pv =>
+                    {
+                        int cost;
+                        if (!this.TryParseStringCost(pv.StringValue, out cost))
+                        {
+                            return false;
+                        }
+
+                        return cost >= this.options.MinCost
+                            && cost <= this.options.MaxCost;
+                    }));
+            }
+        }
+
+        /// <summary>
+        /// Добавить условие поиска по дате (после загрузки объектов).
+        /// </summary>
+        private void AddDatePostCondition()
+        {
+            if (this.options.MinDate != null
+                || this.options.MaxDate != null)
+            {
+                DateTime minDate = this.options.MinDate ?? DateTime.MinValue;
+                DateTime maxDate = this.options.MaxDate ?? DateTime.MaxValue;
+
+                this.currentLoadedResult = this.currentLoadedResult.Where(ro =>
+                    ro.PropertyValues.Any(pv =>
+                        pv.PropertyType.PropertyName == Metadata.DatePropName
+                        && DateTime.Parse(pv.StringValue) >= minDate
+                        && DateTime.Parse(pv.StringValue) <= maxDate));
+            }
+        }
+
+        /// <summary>
+        /// Получить целочисленное значение цены из строкового представления.
+        /// </summary>
+        /// <param name="stringCost">Строковое представление цены.</param>
+        /// <param name="result">Целочисленное представление цены.</param>
+        /// <returns>True - целочисленное значение получено корректно,
+        /// false - <paramref name="stringCost"/> не является строковым
+        /// представлением цены.</returns>
+        private bool TryParseStringCost(string stringCost, out int result)
+        {
+            result = 0;
+
+            if (string.IsNullOrWhiteSpace(stringCost))
+            {
+                return false;
+            }
+
+            stringCost = stringCost.Replace("р", string.Empty);
+            stringCost = stringCost.Replace("тыс", string.Empty);
+            stringCost = stringCost.Replace("руб", string.Empty);
+            stringCost = stringCost.Replace(".", string.Empty);
+
+            return int.TryParse(stringCost, out result);
         }
     }
 }
